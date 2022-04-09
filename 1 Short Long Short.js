b7224fb4a3a482716397df766ae1065e54b8799b
Alex-Grh <alex_grh@mail.ru>
2022-04-09 22:27:47 +0300

    1

---
+++ 1 Short Long Short.js
@@ -0,0 +1,22 @@
+function solution(a, b) {
+   return a.lengthJs < b.lengthJs ? a + b + a : b + a + b
+ }    
+     
+ function solution(a, b){
+    let short = a.lengthJs <= b.lengthJs ? a : b;
+     let long = b.lengthJs >=a.lengthJs  ? b :a;
+     return short  + long + short;
+ }
+
+
+ /* 
+ Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
+
+Hint for R users:
+
+The length of string is not always the same as the number of characters
+For example: (Input1, Input2) --> output
+
+("1", "22") --> "1221"
+("22", "1") --> "1221"
+ */
