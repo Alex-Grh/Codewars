56b016e8b4350d8f7466fc382c6c5729d424628a
Alex-Grh <alex_grh@mail.ru>
2022-04-15 22:22:30 +0300

    1

---
+++ 7 String repeat.js
@@ -0,0 +1,15 @@
+/*
+Write a function called repeatStr which repeats the given string string exactly n times.
+
+repeatStr(6, "I") // "IIIIII"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+*/
+
+function repeatStr (n, s) {
+   if (n > 0)
+     return s.repeat(n);
+   else
+     return "";
+ }
+ repeatStr(6, "I");
+ repeatStr(5, "Hello");
