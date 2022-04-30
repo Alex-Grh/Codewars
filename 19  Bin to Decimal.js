34300a2f81bdb9f3f41beb798ef2ec401877ffe7
Alex-Grh <alex_grh@mail.ru>
2022-04-30 22:11:01 +0400

    1

---
+++ 19  Bin to Decimal.js
@@ -0,0 +1,12 @@
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+function binToDec(bin) {
+   return parseInt((bin + '')
+      .replace(/[^01]/gi, ''), 2);
+}
+
+/*
+Вариант 2
+function binToDec(bin){
+   return parseInt(bin,2);
+ }
+ */
