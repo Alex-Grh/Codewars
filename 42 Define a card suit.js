0aaedd27cc79c0562f2c06e74b36cc9c59b94ac9
Alex-Grh <alex_grh@mail.ru>
2022-05-21 19:32:32 +0400

    1

---
+++ 42 Define a card suit.js
@@ -0,0 +1,29 @@
+/*
+You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+Our deck (is preloaded):
+
+('3♣') -> return 'clubs'
+('3♦') -> return 'diamonds'
+('3♥') -> return 'hearts'
+('3♠') -> return 'spades'
+*/
+
+function defineSuit(card) {
+   let anyCard = card.substr(-1);
+   switch (anyCard) {
+      case '♣':
+         return 'clubs';
+         break;
+      case '♦':
+         return 'diamonds';
+         break;
+      case '♥':
+         return 'hearts';
+         break;
+      case '♠':
+         return 'spades';
+         break;
+      default:
+   }
+}
