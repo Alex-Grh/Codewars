577f77fec7085816326cfecc35ff23ae82c0c63c
Alex-Grh <alex_grh@mail.ru>
2022-05-24 23:17:04 +0400

    1

---
+++ 44 Convert a Number to a String.js
@@ -0,0 +1,21 @@
+/*
+Convert a Number to a String!
+55813887% of 13,75563,190 of 162,085AKJ.IO1 Issue Reported
+ JavaScript
+Node v14.x
+VIM
+EMACS
+Instructions
+Output
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+Examples:
+123 --> "123"
+999 --> "999"
+*/
+function numberToString(num) {
+   return num = String(num);
+
+}
