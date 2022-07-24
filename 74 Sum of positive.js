b4747388b06d8e459ad67daf22c2b4ad27af123f
Alex-Grh <alex_grh@mail.ru>
2022-07-24 21:59:47 +0400

    1

--- 74 Sum of positive.js
+++ 74 Sum of positive.js
@@ -17,11 +17,22 @@ function positiveSum(arr) {
 
 // Вариант 2
 function positiveSum(arr) {
-   var total = 0;    
+   var total = 0;
    for (i = 0; i < arr.length; i++) {    // setup loop to go through array of given length
-     if (arr[i] > 0) {                   // if arr[i] is greater than zero
-       total += arr[i];                  // add arr[i] to total
-     }
+      if (arr[i] > 0) {                   // if arr[i] is greater than zero
+         total += arr[i];                  // add arr[i] to total
+      }
    }
    return total;                         // return total
- }
+}
+
+// Вариант 2
+function positiveSum(arr) {
+   let sum = 0;
+   for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > 0) {
+         sum += arr[i];
+      }
+   }
+   return sum
+}
