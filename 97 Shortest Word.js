95e037b7df38e83297a28c00e84e7e2e4ae21cab
Alex-Grh <alex_grh@mail.ru>
2022-08-29 23:52:51 +0400

    1

--- 97 Shortest Word.js
+++ 97 Shortest Word.js
@@ -3,3 +3,14 @@ Simple, given a string of words, return the length of the shortest word(s).
 
 String will never be empty and you do not need to account for different data types.
 */
+
+function findShort(s){
+   let arr = s.split(' ');
+   let smal = arr[0];
+   for (let i = 0; i < arr.length; i++) {
+     if(arr[i].length < smal.length){
+       smal = arr[i];
+     }
+   }
+   return smal.length;
+ }
